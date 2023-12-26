dcfa258fc8312ee03767cea8510fb490226d28cd
Jianzhao Gao <nk_pi@hotmail.com>
2023-12-26 10:02:20 +0800

    Update example.R

--- example.R
+++ example.R
@@ -1,19 +1,14 @@
-source("/BDGRN/code/BDGRN.R")
-source("/BDGRN/code/compute.R")
-# 读取单细胞表达数据gene*cell
+source("./code/BDGRN.R")
+source("./code/compute.R")
 
-data = read.csv("/BDGRN/data/example/ExpressionData.csv",header =T ,row.names =1)
-##读取细胞时间序列
 
-time = read.csv("/BDGRN/data/example/PseudoTime.csv",header=T,row.names =1)
- ## 计算得到网络的edgelist及probability
-##可自行设置参数 k, cutoff, w
+# reading in scRNA-seq dataset gene*cell
+data = read.csv("./data/example/ExpressionData.csv",header =T ,row.names =1)
+
+# reading the PseudoTime 
+time = read.csv("./data/example/PseudoTime.csv",header=T,row.names =1)
+
+#run BDGRN
 edgelist = BDGRN(data,time,k=3,cutoff = 0, w =0.1)
-edgelist
+print(edgelist)
 
-##compute AUPR/AUROC/Early Precision
-##导入ground-truth network
-test<-read.csv("/BDGRN/data/example/refNetwork.csv",header=T)
-
-res = comAUPR(test, edgelist)
-EP = comEP(test, edgelist)
